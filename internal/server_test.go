e87a55f96be166b5a27fa72b1e3ac18855e842fb
Flavio Garcia <piraz@candango.org>
2023-06-03 01:07:10 -0400

    chore(server): add nonced service and handler
    Refs: #2

--- internal/server_test.go
+++ internal/server_test.go
@@ -1,6 +1,7 @@
 package peasant
 
 import (
+	"log"
 	"net/http"
 	"testing"
 
@@ -8,24 +9,70 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func hFunc(res http.ResponseWriter, req *http.Request) {
-	response := "A test"
-	_, err := res.Write([]byte(response))
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
+type InMemoryNonceService struct {
+	nonceMap map[string]*interface{}
+}
+
+func (s *InMemoryNonceService) Block(resp http.ResponseWriter,
+	req *http.Request) error {
+	return nil
+}
+func (s *InMemoryNonceService) Clear(nonce string) error {
+	return nil
+}
+func (s *InMemoryNonceService) Consume(resp http.ResponseWriter,
+	req *http.Request, nonce string) (bool, error) {
+	return false, nil
+}
+func (s *InMemoryNonceService) GetNonce(req *http.Request) (string, error) {
+	return "", nil
+}
+func (s *InMemoryNonceService) provided(req http.ResponseWriter,
+	res *http.Request) (bool, error) {
+	return false, nil
+}
+
+type NoncedHandler struct {
+	http.Handler
+	log.Logger
+	service NonceService
+}
+
+func NewNoncedHandler() *NoncedHandler {
+	h := http.NewServeMux()
+	h.Handle("/new-nonce", &NonceHandler{})
+	return &NoncedHandler{
+		Handler: h,
+	}
+
+}
+
+type NonceHandler struct {
+	service NonceService
+}
+
+func (h *NonceHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	method := req.Method
+	if method != http.MethodHead {
+		res.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+	res.Header().Add("return", "OK")
 }
-func TestWithHandler(t *testing.T) {
-	runner := testrunner.NewHttpTestRunner(t)
 
-	t.Run("With func, clear func after", func(t *testing.T) {
-		res, err := runner.WithFunc(hFunc).ClearFuncAfter().Get()
-		if err != nil {
-			t.Error(err)
-		}
-		response := testrunner.BodyAsString(t, res)
-		assert.Equal(t, "200 OK", res.Status)
-		assert.Equal(t, response, "A test")
+func TestServer(t *testing.T) {
+	runner := testrunner.NewHttpTestRunner(t).WithHandler(NewNoncedHandler())
+
+	t.Run("Head Request tests", func(t *testing.T) {
+
+		t.Run("Request OK", func(t *testing.T) {
+			res, err := runner.WithPath("/new-nonce").Head()
+			if err != nil {
+				t.Error(err)
+			}
+			assert.Equal(t, "200 OK", res.Status)
+			assert.Equal(t, http.NoBody, res.Body)
+			assert.Equal(t, "OK", res.Header.Get("return"))
+		})
 	})
 }
