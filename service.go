d516189d5af45cf843b84945cc5c1d45e6dbca3b
Flavio Garcia <piraz@candango.org>
2024-06-05 23:59:29 -0400

    feat(service): introduce NoncedHandlerFunc
    The NoncedHandlerFunc recives a nonce service and a handler
    function to be executed only if the nonce is provided in
    the header.
    
    Http error returning should be implemented in the nonce service.
    
    Refs: #2

--- service.go
+++ service.go
@@ -2,14 +2,74 @@ package peasant
 
 import "net/http"
 
+// NonceService defines methods for managing nonces in HTTP requests.
+// It provides functionality for blocking, clearing, consuming, getting,
+// and checking the provision of nonces.
 type NonceService interface {
+
+	// Block blocks the provided HTTP request if the nonce is not valid.
 	Block(http.ResponseWriter, *http.Request) error
+
+	// Clear clears a nonce associated with the specified key. If the key
+	// doesn't exists no error will be returned.
+	//
+	// Return errors only if an actual error occours.
 	Clear(string) error
-	Consume(http.ResponseWriter, *http.Request, string) (bool, error)
+
+	// Consume processes the nonce associated with the specified key and
+	// returns a boolean indicating whether the nonce was successfully
+	// consumed, along with any error encountered.
+	// If the specified key is not present in the map, the method will return
+	// false without performing any action.
+	Consume(http.ResponseWriter, *http.Request) (bool, error)
+
+	// GetNonce generates a new nonce, and stores it for a future validation.
+	// It returns the nonce as a string and an error if any occurred during
+	// the nonce generation or header update.
 	GetNonce(*http.Request) (string, error)
+
+	// Provided verifies the presence of a valid nonce in the specified HTTP
+	// request.
+	//
+	// If the nonce is not provided or is invalid, it sets the response HTTP
+	// status to "Unauthorized", "Forbidden", or another appropriate status
+	// based on the specific conditions and checks performed within the method.
+	// It returns a boolean indicating whether the nonce was provided and
+	// valid, along with any error encountered.
 	Provided(http.ResponseWriter, *http.Request) (bool, error)
 }
 
+func NoncedHandlerFunc(s NonceService,
+	f func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		ok, err := s.Provided(res, req)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if !ok {
+			return
+		}
+		ok, err = s.Consume(res, req)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if !ok {
+			return
+		}
+		nonce, err := s.GetNonce(req)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		res.Header().Add("nonce", nonce)
+		f(res, req)
+	}
+}
+
+// Nonced processes the provided HTTP request to verify and consume a
+// valid nonce. It returns an error if any occurred during the process.
 func Nonced(res http.ResponseWriter, req *http.Request,
 	service NonceService) (err error) {
 	ok, err := service.Provided(res, req)
@@ -28,7 +88,7 @@ func Nonced(res http.ResponseWriter, req *http.Request,
 		return err
 	}
 
-	ok, err = service.Consume(res, req, nonce)
+	ok, err = service.Consume(res, req)
 	if err != nil {
 		return err
 	}
--- service_test.go
+++ service_test.go
@@ -22,8 +22,11 @@ func randomString(s int) string {
 	return string(r)
 }
 
+// InMemoryNonceService implements the NonceService interface for managing
+// nonces in an in-memory map.
 type InMemoryNonceService struct {
 	nonceMap map[string]*interface{}
+	t        *testing.T
 }
 
 func (s *InMemoryNonceService) Block(resp http.ResponseWriter,
@@ -31,6 +34,8 @@ func (s *InMemoryNonceService) Block(resp http.ResponseWriter,
 	return nil
 }
 
+// Clear clears the nonce associated with the specified key in the in-memory
+// map.
 func (s *InMemoryNonceService) Clear(nonce string) error {
 	_, ok := s.nonceMap[nonce]
 	if !ok {
@@ -40,10 +45,18 @@ func (s *InMemoryNonceService) Clear(nonce string) error {
 	return nil
 }
 
-func (s *InMemoryNonceService) Consume(resp http.ResponseWriter,
-	req *http.Request, nonce string) (bool, error) {
+// Consume consumes the nonce associated with a specified key and returns
+// whether the nonce was successfully consumed and any error that occurred.
+func (s *InMemoryNonceService) Consume(res http.ResponseWriter,
+	req *http.Request) (bool, error) {
+	nonce := req.Header.Get("nonce")
+	if nonce == "" {
+		res.WriteHeader(http.StatusForbidden)
+		return false, nil
+	}
 	_, ok := s.nonceMap[nonce]
 	if !ok {
+		res.WriteHeader(http.StatusForbidden)
 		return false, nil
 	}
 	err := s.Clear(nonce)
@@ -56,7 +69,7 @@ func (s *InMemoryNonceService) Consume(resp http.ResponseWriter,
 func (s *InMemoryNonceService) GetNonce(req *http.Request) (string, error) {
 	nonce := randomString(32)
 	s.nonceMap[nonce] = nil
-	ticker := time.NewTicker(2000 * time.Millisecond)
+	ticker := time.NewTicker(250 * time.Millisecond)
 	done := make(chan bool)
 
 	go func(nonce string) {
@@ -74,9 +87,14 @@ func (s *InMemoryNonceService) GetNonce(req *http.Request) (string, error) {
 	return nonce, nil
 }
 
-func (s *InMemoryNonceService) Provided(_ http.ResponseWriter,
-	_ *http.Request) (bool, error) {
-	return false, nil
+func (s *InMemoryNonceService) Provided(res http.ResponseWriter,
+	req *http.Request) (bool, error) {
+	nonce := req.Header.Get("nonce")
+	if nonce == "" {
+		res.WriteHeader(http.StatusForbidden)
+		return false, nil
+	}
+	return true, nil
 }
 
 type NoncedHandler struct {
@@ -84,20 +102,22 @@ type NoncedHandler struct {
 	service NonceService
 }
 
-func NewNoncedHandler() *NoncedHandler {
-	h := http.NewServeMux()
-	h.Handle("/new-nonce", &NoncedHandler{})
+func NewNoncedServeMux(t *testing.T) *http.ServeMux {
 	s := &InMemoryNonceService{
 		nonceMap: make(map[string]*interface{}),
+		t:        t,
 	}
-	return &NoncedHandler{
-		Handler: h,
+	nonced := &NoncedHandler{
 		service: s,
 	}
-
+	h := http.NewServeMux()
+	h.HandleFunc("/new-nonce", nonced.getNonce)
+	h.HandleFunc("/do-nonced-something",
+		NoncedHandlerFunc(s, nonced.doNoncedFunc))
+	return h
 }
 
-func (h *NoncedHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+func (h *NoncedHandler) getNonce(res http.ResponseWriter, req *http.Request) {
 	method := req.Method
 	if method != http.MethodHead {
 		res.WriteHeader(http.StatusMethodNotAllowed)
@@ -111,12 +131,26 @@ func (h *NoncedHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	res.Header().Add("nonce", nonce)
 }
 
-func TestServer(t *testing.T) {
-	runner := testrunner.NewHttpTestRunner(t).WithHandler(NewNoncedHandler())
+func (h *NoncedHandler) doNoncedFunc(res http.ResponseWriter, req *http.Request) {
+	method := req.Method
+	if method != http.MethodGet {
+		res.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	nonce := req.Header.Get("nonce")
+	res.Write([]byte("Func done with nonce " + nonce))
+}
 
-	t.Run("Head Request tests", func(t *testing.T) {
+func TestServer(t *testing.T) {
+	runner := testrunner.NewHttpTestRunner(t).WithHandler(NewNoncedServeMux(t))
+
+	t.Run("Retrieve a new nonce", func(t *testing.T) {
 		t.Run("Request OK", func(t *testing.T) {
-			res, err := runner.WithPath("/new-nonce").Head()
+			res, err := runner.WithPath("/new-nonce").Get()
+			assert.Equal(t, "405 Method Not Allowed", res.Status)
+			assert.Equal(t, http.NoBody, res.Body)
+
+			res, err = runner.WithPath("/new-nonce").Head()
 			if err != nil {
 				t.Error(err)
 			}
@@ -124,9 +158,43 @@ func TestServer(t *testing.T) {
 			assert.Equal(t, "200 OK", res.Status)
 			assert.Equal(t, http.NoBody, res.Body)
 			assert.Equal(t, 32, len(res.Header.Get("nonce")))
-			res, err = runner.WithPath("/new-nonce").Get()
-			assert.Equal(t, "405 Method Not Allowed", res.Status)
-			assert.Equal(t, http.NoBody, res.Body)
+		})
+	})
+
+	t.Run("Run a nonced function", func(t *testing.T) {
+		t.Run("Request OK", func(t *testing.T) {
+			res, err := runner.WithPath("/new-nonce").Head()
+			if err != nil {
+				t.Error(err)
+			}
+			nonce := res.Header.Get("nonce")
+
+			res, err = runner.WithPath(
+				"/do-nonced-something").WithHeader("nonce", nonce).Get()
+			if err != nil {
+				t.Error(err)
+			}
+
+			assert.Equal(t, "200 OK", res.Status)
+			assert.Equal(t, "Func done with nonce "+nonce,
+				testrunner.BodyAsString(t, res))
+			assert.Equal(t, 32, len(res.Header.Get("nonce")))
+		})
+		t.Run("Expired nonce", func(t *testing.T) {
+			res, err := runner.WithPath("/new-nonce").Head()
+			if err != nil {
+				t.Error(err)
+			}
+			nonce := res.Header.Get("nonce")
+
+			time.Sleep(250 * time.Millisecond)
+			res, err = runner.WithPath(
+				"/do-nonced-something").WithHeader("nonce", nonce).Get()
+			if err != nil {
+				t.Error(err)
+			}
+
+			assert.Equal(t, "403 Forbidden", res.Status)
 		})
 	})
 }
